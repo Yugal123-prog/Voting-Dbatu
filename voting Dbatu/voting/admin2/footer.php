47978fc42cc43e4ee3e2296ddb2f78199180e100
yugal patil <y.patilYugal226@gmail.com>
2023-06-03 10:46:03 +0530

    Second commit

---
+++ voting Dbatu/voting/admin2/footer.php
@@ -0,0 +1,12 @@
+
+<footer class="footer">
+	<hr/>
+&copy2018. 
+<br/>
+Online Voting System.
+<br/>
+University Of Dbatu
+<br/>
+
+
+</footer>
---
+++ voting Dbatu/voting/view_banner.php
@@ -0,0 +1,34 @@
+<style type="text/css">
+nav ul li{
+    display: inline-block;
+    padding-left: 20px;
+    padding-bottom: 10px;
+}
+nav ul li:hover{
+    text-decoration: none;
+}
+a:hover{
+    text-decoration: none;
+}
+
+
+</style>
+    	
+    
+            <div class="text-center text-success animateuse" style="background-color:rgba(0, 100, 0, 1); font-size:25px;text-align:center; margin-top:-10px; padding-top:20px;">
+            <img src="img/DBATU.png" width="5%" hieght="5%" />   <a style="color:white; font-weight:bold; font-size:30px;">Student Union Elections 2023</a>
+              
+        <nav class="nav-menue">
+            <ul style = "color">
+                <li>
+                    <a href="index.php">Home</a>
+                </li>
+                <li><a href="candidate_path.php">Candidates</a></li>
+                <li><a href="unilusu.php">UNILUSU</a></li>
+                <li> <a href="register/index.php">Register</a></li>
+                <li><a href="voters.php">Voter List</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
+					
+</div>
